af9891ed7e6a2eaa4db3a54ad1e35148d72198fa
poemDead <69440589+poemDead@users.noreply.github.com>
2020-10-19 16:13:33 +0900

    简陋的首页&修改model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """返回模型的字符串表示"""
@@ -14,13 +14,15 @@ class Entry(models.Model):
     """关于某主题的具体知识的一个条目"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         # 多个条目显示为Entries 而不是默认的Entrys
         verbose_name_plural = 'Entries'
     
     def __str__(self):
-        return f"{self.text[:50]}..."
-
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
     
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+def index(request):
+    """学习笔记应用的主页"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics,}
+    return render(request, 'learning_logs/index.html', context)
