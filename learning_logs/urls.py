d5fb1e603bb989e6ab990436b8acb4dceae13650
poemDead <69440589+poemDead@users.noreply.github.com>
2020-10-20 23:05:57 +0900

    添加新主题功能

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # topic page
     path('topics/<int:topic_id>', views.topic, name='topic'),
+    # new topic page
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,18 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # create a new form
+        form = TopicForm()
+    else:
+        # 如果是post请求，就提交表单处理数据，跳回topics界面
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:index')
+    #如果没有post请求，就显示空表单
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
